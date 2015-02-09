fca1a5c9f64004a4c3803edd8c6d573115cdf9b9
evanjf <evanjfrancis@gmail.com>
2015-02-09 14:13:24 -0500

    initial commit. working functionality

---
+++ README.md
@@ -0,0 +1,107 @@
+# envpreprocess
+
+> preprocess environment variables
+
+## Getting Started
+This plugin requires Grunt `~0.4.5`
+
+If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins. Once you're familiar with that process, you may install this plugin with this command:
+
+```shell
+npm install envpreprocess --save-dev
+```
+
+Once the plugin has been installed, it may be enabled inside your Gruntfile with this line of JavaScript:
+
+```js
+grunt.loadNpmTasks('envpreprocess');
+```
+
+## The "envpreprocess" task
+A task for replacing environment-specific variables in a specified file or directory. Environment variables are specified in an external .json file. Text search and replacement in files is powered by the core of [grunt-text-replace](https://github.com/yoniholmes/grunt-text-replace).
+
+### Overview
+In your project's Gruntfile, add a section named `envpreprocess` to the data object passed into `grunt.initConfig()`.
+
+```js
+grunt.initConfig({
+   envpreprocess:{
+        dev:{
+            files:{
+                
+            },
+            options:{
+                replacePath: ['dev_build/**/*.*'],
+                environment: 'dev'
+            }
+        },
+        prod:{
+            files:{
+                src:  'config/env.json',
+            },
+            options:{
+                replacePath: ['dist_build/**/*.*'],
+                environment: 'prod'
+            }
+        }
+    }
+});
+```
+When the task is run, you'll see a log output like this
+```
+Running "envpreprocess:prod" (envpreprocess) task
+Reading ENV variables from config/env.json
+Replaced all ENV variables in dev_build/**/*.*
+```
+
+### Options
+
+#### options.replacePath
+Type: `String`
+
+The path of files to have ENV variables replaced with the preprocessor.
+
+#### options.environment
+Type: `String`
+Default value: `dev'`
+
+Which environment to use when retrieving ENV variables from the .json file. If given "*", any environment that isn't specified will use that value.
+
+
+```
+
+#### Example Usage
+Environment variables are defined in the config file located at the specified 'files.src' file. For each variable you must define a value for each environment, or use "*" to apply it to any environments that don't have a value specified. This is an example of such file
+```json
+{
+	"APP_NAME": {
+		"dev": "AppDev",
+		"prod": "AppProd"
+	},
+	"APP_VERSION": {
+        "*"
+	},
+    "API_BASE": {
+        "dev": "http://localhost:8000",
+        "prod": "https://www.mysite.com/api"
+    }
+}
+
+Then in a file you want an ENV variable to be replaced, use something like this
+
+```html
+    <head>
+        <title>ENV.APP_NAME</title>
+    </head>
+    <script src="ENV.API_BASE/user/create">
+     ENV.APP_VERSION
+```
+
+```js
+    var version = "ENV.APP_VERSION";
+    alert(version);
+```
+
+
+## Release History
+0.1.0 - Initial release. Functionality is working. Not tested.
---
+++ tasks/envpreprocess.js
@@ -0,0 +1,119 @@
+/*
+ * envpreprocess
+ * https://github.com/Evan/grunt_plugin_test
+ *
+ * Copyright (c) 2015 evanjf
+ * Licensed under the MIT license.
+ */
+
+/*
+
+    options format:
+    options: {
+        configPath: 'config/env.json',
+        srcDir: 'app/'
+    }
+
+*/
+
+'use strict';
+
+var textReplace = require('../grunt-text-replace');
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('envpreprocess', 'preprocess environment variables', function() {      
+      
+    //default environment to 'dev', and grab destination
+    var environment = "dev",
+        dest = this.data.options.replacePath;
+        //dest = this.data.files.dest;
+      
+          //read environment parameter passed in
+    if(this.data.options.environment){
+        environment = this.data.options.environment;
+    }
+      
+    var optionsForReplace = this.options({
+        src: [dest],
+        overwrite:true,
+        replacements: []
+    });
+      
+    this.files.forEach(function(f) {
+        //grab src
+        var src = f.src.filter(function(filepath) {
+            // Warn on and remove invalid source files (if nonull was set).
+            if (!grunt.file.exists(filepath)) {
+              grunt.log.warn('Source file "' + filepath + '" not found.');
+              return false;
+            } else {
+              return true;
+            }
+        });
+        
+        grunt.log.writeln("Reading ENV variables from " + src[0]);
+
+        //load config into object from src
+        var config = grunt.file.readJSON(src[0]);
+        var replacementsArr = [];
+        
+        //iterate over each config variable key
+        var configKeys = Object.keys(config).forEach(function(key){
+            var val = null,
+                warned = false;
+            //grab value and string to replace
+            if(!config[key][environment]){
+                //check for a "*" if no matching environment var
+                if(!config[key]["*"]){
+                    grunt.log.warn("WARNING: No corresponding ENV variable for " + key);   
+                    warned = true;
+                }else{
+                    val = config[key]["*"];
+                }
+            }
+            //grab corresponding ENV var
+            if(val == null ){
+               val = config[key][environment];  
+            }
+            if(val == null){
+                if(!warned){
+                    grunt.log.warn("WARNING: No corresponding ENV variable for " + key); 
+                }
+            }
+            var replaceString = "ENV." + key;
+
+            //add to replacements array
+            replacementsArr.push({
+                from: replaceString,
+                to: val
+            });
+            
+           
+        });
+
+        optionsForReplace.replacements = replacementsArr;
+        try{
+            textReplace.replace({
+                src: dest,
+                overwrite: true,
+                replacements:replacementsArr
+              });
+            grunt.log.writeln("Replaced all ENV variables in " + dest);
+
+        }
+        catch(err){
+            grunt.log.error("ERROR: Envpreprocess replace error: " + err);   
+        }
+    });
+  });
+
+};
+
+    /*"grunt-text-replace" : "~0.4.0",
+    "extend-grunt-plugin" : "~0.1.0",
+    "recursive-readdir":"~1.2.1",*/
+
